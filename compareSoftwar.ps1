30781ec0c53dda70b95531e9edc9a890108c6a60
Jonathan Schmidt <78372510+SchmidtJonathan1@users.noreply.github.com>
2021-09-21 14:56:33 +0200

    Update compareSoftwar.ps1

--- compareSoftwar.ps1
+++ compareSoftwar.ps1
@@ -2,11 +2,11 @@ $Pathx86Objs = Get-ItemProperty 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\Cu
 $Pathx64Objs = Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*'
 
 $CustomObjs = @()
-$CustomObjs += New-Object -TypeName psobject -Property @{Name="KeePass Password Safe 2.49"; Version="2.49"; 'InstallationFoundx86'= $null; 'InstallationFoundx64' = $null; 'needToUpdate' = $null}
-$CustomObjs += New-Object -TypeName psobject -Property @{Name="Mozilla Maintenance Service"; Version="92.0"; 'InstallationFoundx86'= $null; 'InstallationFoundx64' = $null; 'needToUpdate' = $null}
-$CustomObjs += New-Object -TypeName psobject -Property @{Name="Mozilla Firefox (x64 en-US)"; Version="92.0"; 'InstallationFoundx86'= $null; 'InstallationFoundx64' = $null; 'needToUpdate' = $null}
-$CustomObjs += New-Object -TypeName psobject -Property @{Name="draw.io 15.2.7"; Version="15.2.7"; 'InstallationFoundx86'= $null; 'InstallationFoundx64' = $null; 'needToUpdate' = $null}
-$CustomObjs += New-Object -TypeName psobject -Property @{Name="MobaXterm"; Version="21.3.0.4736"; 'InstallationFoundx86'= $null; 'InstallationFoundx64' = $null; 'needToUpdate' = $null}
+$CustomObjs += New-Object -TypeName psobject -Property @{Name="KeePass Password Safe 2.49"; Version="2.49"; 'InstallationFoundx86'= $false; 'InstallationFoundx64' = $false; 'needToUpdate' = $null}
+$CustomObjs += New-Object -TypeName psobject -Property @{Name="Mozilla Maintenance Service"; Version="92.0"; 'InstallationFoundx86'= $false; 'InstallationFoundx64' = $false; 'needToUpdate' = $null}
+$CustomObjs += New-Object -TypeName psobject -Property @{Name="Mozilla Firefox (x64 en-US)"; Version="92.0"; 'InstallationFoundx86'= $false; 'InstallationFoundx64' = $false; 'needToUpdate' = $null}
+$CustomObjs += New-Object -TypeName psobject -Property @{Name="draw.io 15.2.7"; Version="15.2.7"; 'InstallationFoundx86'= $false; 'InstallationFoundx64' = $false; 'needToUpdate' = $null}
+$CustomObjs += New-Object -TypeName psobject -Property @{Name="MobaXterm"; Version="21.3.0.4736"; 'InstallationFoundx86'= $false; 'InstallationFoundx64' = $false; 'needToUpdate' = $null}
 
 foreach($CustomObj in $CustomObjs){
   foreach($Pathx86Obj in $Pathx86Objs){
