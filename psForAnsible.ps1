b70e79d98dee7a001d895fec32514dbd0127c8b4
Jonathan Schmidt <78372510+SchmidtJonathan1@users.noreply.github.com>
2021-09-24 10:37:04 +0200

    Update psForAnsible.ps1

--- psForAnsible.ps1
+++ psForAnsible.ps1
@@ -7,7 +7,7 @@ param([Parameter(Mandatory)][string]$SoftwareName,
 ## Set variable
 # Regpath
 $Pathx86Objs = Get-ItemProperty -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
-$Pathx64Objs = Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*'
+$Pathx64Objs = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*'
 
 #Set yout software installpath
 $InstallPath = Get-ChildItem -Path $FolderPath
@@ -21,15 +21,15 @@ $CustomObjs = @()
 $SplitName = ($SoftwareName -split ' ')[0]
 foreach($Pathx86Obj in $Pathx86Objs){
     if($Pathx86Obj.DisplayName.Contains($SplitName)){         
-        $CheckRegName = $Pathx86Obj.DisplayName
-        $CustomObjs += New-Object -TypeName psobject -Property @{'Name'=$CheckRegName ; 'Version'=$SoftwareVersion; 'InstallationFoundx86'= $null; 'InstallationFoundx64' = $null; 'NeedToUpdate' = $null; 'InstallParam'=$SoftwareParam}
+        $CheckRegName86 = $Pathx86Obj.DisplayName
+        $CustomObjs += New-Object -TypeName psobject -Property @{'Name'=$CheckRegName86 ; 'Version'=$SoftwareVersion; 'InstallationFoundx86'= $null; 'InstallationFoundx64' = $null; 'NeedToUpdate' = $null; 'InstallParam'=$SoftwareParam}
     }
 }
 
 foreach($Pathx64Obj in $Pathx64Objs){
-    if($Pathx64Obj.DisplayName.Contains($SplitName)){         
-        $CheckRegName = $Pathx64Obj.DisplayName
-        $CustomObjs += New-Object -TypeName psobject -Property @{'Name'=$CheckRegName ; 'Version'=$SoftwareVersion; 'InstallationFoundx86'= $null; 'InstallationFoundx64' = $null; 'NeedToUpdate' = $null; 'InstallParam'=$SoftwareParam}
+    if($Pathx64Obj.DisplayName.Contains($SplitName)){     
+        $CheckRegName64 = $Pathx64Obj.DisplayName
+        $CustomObjs += New-Object -TypeName psobject -Property @{'Name'=$CheckRegName64 ; 'Version'=$SoftwareVersion; 'InstallationFoundx86'= $null; 'InstallationFoundx64' = $null; 'NeedToUpdate' = $null; 'InstallParam'=$SoftwareParam}
     }
 }
 
@@ -41,9 +41,9 @@ foreach($CustomObj in $CustomObjs){
                 $CustomObj.InstallationFoundx86 = $true
                 if($Pathx86Obj.DisplayVersion -lt $CustomObj.Version){
                     $CustomObj.NeedToUpdate = $true   
+                }
             }
         }
-     }
     $CustomObj.InstallationFoundx64 = $false
         foreach($Pathx64Obj in $Pathx64Objs){
             if($Pathx64Obj.DisplayName -eq $CustomObj.Name){
@@ -55,14 +55,14 @@ foreach($CustomObj in $CustomObjs){
         }
 
 # Output hastable
-    $CustomObj
+    $CustomObjs
 
-# Install software if update -eq true
+#Install software if update -eq true
     foreach($CustomObjName in $CustomObj.Name){
         $SplitName = ($CustomObjName -split ' ')[0]
         foreach($Installpathobj in $InstallPath){
             if($InstallPathObj.BaseName.Contains($SplitName) -and ($CustomObj.NeedToUpdate -eq $true)){
-                Start-Process -FilePath $InstallPathObj.FullName -ArgumentList $CustomObj.InstallParam
+                #Start-Process -FilePath $InstallPathObj.FullName -ArgumentList $CustomObj.InstallParam
                 Write-Host "$SplitName installed" -ForegroundColor Green
             }
         }
